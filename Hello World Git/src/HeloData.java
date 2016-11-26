fba4cd3e3d6154724e7137482fefc40669882e59
DenisIzmestyev <supelmont@gmail.com>
2016-11-26 16:30:36 +0300

    2

--- Hello World Git/src/HeloData.java
+++ Hello World Git/src/HeloData.java
@@ -1,9 +1,9 @@
-
+import java.util.*;
 public class HeloData {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Привет сегодня: ");
 	}
 
 }
